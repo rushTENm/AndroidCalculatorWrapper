ed2a47634d37c8e2d960bba84db79b7d7f88759c
rushTENm <rushtenm@gmail.com>
2017-04-29 14:18:03 +0900

    Initial commit

---
+++ app/src/main/java/com/example/rusht/androidcalculatorwrapper/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rusht.androidcalculatorwrapper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
