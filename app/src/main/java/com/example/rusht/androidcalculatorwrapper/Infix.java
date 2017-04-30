5a19be3d5a02bb7dde75c176a9c64855a5fd7dd6
rushTENm <rushtenm@gmail.com>
2017-04-30 22:40:42 +0900

    Works

---
+++ app/src/main/java/com/example/rusht/androidcalculatorwrapper/Infix.java
@@ -0,0 +1,63 @@
+package com.example.rusht.androidcalculatorwrapper;
+
+import java.util.Stack;
+
+public class Infix {
+    char ch, topOp;
+    Stack temp = new Stack();
+    String result = "";
+
+    public Infix(String strInfix) {
+        for (int i = 0; i < strInfix.length(); i++) {
+            ch = strInfix.charAt(i);
+
+            switch (ch) {
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                    while (!temp.isEmpty() && (precede(ch) <= precede(temp.peek().toString().charAt(0)))) {
+                        result += temp.pop().toString();
+                    }
+                    temp.push(ch);
+                    break;
+                case '(':
+                    temp.push(ch);
+                    break;
+                case ')':
+                    topOp = temp.pop().toString().charAt(0);
+                    while (topOp != '(') {
+                        result += topOp;
+                        topOp = temp.pop().toString().charAt(0);
+                    }
+                    break;
+                default:
+                    result += ch;
+                    break;
+            }
+
+        }
+        while (!temp.isEmpty()) {
+            result += temp.pop().toString();
+        }
+    }
+
+    int precede(char op) {
+        switch (op) {
+            case '(':
+            case ')':
+                return 0;
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+        }
+        return -1;
+    }
+
+    String getResult() {
+        return result;
+    }
+}
--- app/src/main/java/com/example/rusht/androidcalculatorwrapper/MainActivity.java
+++ app/src/main/java/com/example/rusht/androidcalculatorwrapper/MainActivity.java
@@ -1,13 +1,65 @@
 package com.example.rusht.androidcalculatorwrapper;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when the user taps the Send button
+     */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void num7(View view) {
+        TextView textView = (TextView) findViewById(R.id.calText);
+        CharSequence charSequence = textView.getText();
+        charSequence = charSequence + "7";
+        textView.setText(charSequence);
+    }
+
+    public void opDel(View view) {
+        TextView textView = (TextView) findViewById(R.id.calText);
+        CharSequence charSequence = textView.getText();
+        String deleted = "";
+        for (int i = 0; i < charSequence.length() - 1; i++) {
+            deleted += charSequence.charAt(i);
+        }
+        textView.setText(deleted);
+    }
+
+    public void opDiv(View view) {
+        TextView textView = (TextView) findViewById(R.id.calText);
+        CharSequence charSequence = textView.getText();
+        charSequence = charSequence + "/";
+        textView.setText(charSequence);
+    }
+
+    public void opEqu(View view) {
+        TextView textView = (TextView) findViewById(R.id.calText);
+        CharSequence charSequence = textView.getText();
+
+        Infix obj = new Infix(charSequence.toString());
+        String postfix = obj.getResult();
+        Postfix obj2 = new Postfix(postfix);
+
+        textView.setText(String.valueOf(obj2.getValue()));
     }
 }
